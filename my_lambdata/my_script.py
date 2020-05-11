175463d25d6deb1ecc24191903923cf32c400f61
Curtis Cecil <curtcecil@gmail.com>
2020-05-10 20:20:22 -0500

    Revert "Oop"

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -8,7 +8,7 @@ from my_lambdata.my_mod import checknan
 
 print("HELLO")
 
-df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+df = DataFrame({"a":[1,2,3], "b":[4,5,6]})
 print(df.head())
 
 x = 11
@@ -16,4 +16,3 @@ print(enlarge(x))
 
 z = int(input("Please choose a number (e.g. 5): "))
 print(decimate(z))
-
